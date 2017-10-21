f19bc27ad246988dba3b4cfe4c33abbf2da6787d
Jesse A. Tov <jesse.tov@gmail.com>
2017-10-21 03:05:38 -0500

    Theorem, lemma, and exercise syntax.

--- notes/util.rkt
+++ notes/util.rkt
@@ -2,25 +2,30 @@
 
 (provide with-rewriters
          term langname rulename
+         theorem lemma exercise
          render-reduction-rules
          render-judgment-rules types/r
          render-nonterminals)
 
-(require racket/list
-         redex/pict
+(require redex/pict
          scribble/base
          (only-in racket/match match-define match-lambda)
-         (only-in redex default-language))
+         (only-in redex default-language)
+         (for-syntax racket/base syntax/parse))
 
 #;
 (define (with-rewriters/thunk thunk)
   (with-compound-rewriters
-   (['lookup (match-lambda [`(,_ ,_ ,Γ ,x ,_) (list "" Γ "(" x ")")])]
-    ['extend (match-lambda [(list _ _ Γ x t _) (list "" Γ ", " x ":" t)])]
+   (['size   (match-lambda [(list _ _ e _)       (list "|" e "|")])]
+    ['meta-+ (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " + " e_2)])]
+    ['meta-* (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " × " e_2)])]
+    ['-->    (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " → " e_2)])]
+    ['lookup (match-lambda [(list _ _ Γ x _)     (list "" Γ "(" x ")")])]
+    ['extend (match-lambda [(list _ _ Γ x t _)   (list "" Γ ", " x ":" t)])]
     ['substitute
-     (match-lambda [(list _ _ e x v _) (list "" e "[" x ":=" v "]")])]
-    ['types* (match-lambda [(list _ _ e t _) (list "" e " : " t)])]
-    ['types  (match-lambda [(list _ _ Γ e t _) (list "" Γ " ⊢ " e " : " t)])])
+             (match-lambda [(list _ _ e x v _)   (list "" e "[" x ":=" v "]")])]
+    ['types* (match-lambda [(list _ _ e t _)     (list "" e " : " t)])]
+    ['types  (match-lambda [(list _ _ Γ e t _)   (list "" Γ " ⊢ " e " : " t)])])
    (with-atomic-rewriter 't "τ" (thunk))))
 
 #;
@@ -29,11 +34,14 @@
 
 (define-syntax-rule (with-rewriters expr0 expr ...)
   (with-compound-rewriters
-   (['-->    (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " → " e_2)])]
+   (['size   (match-lambda [(list _ _ e _)       (list "|" e "|")])]
+    ['meta-+ (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " + " e_2)])]
+    ['meta-* (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " × " e_2)])]
+    ['-->    (match-lambda [(list _ _ e_1 e_2 _) (list "" e_1 " → " e_2)])]
     ['lookup (match-lambda [(list _ _ Γ x _)     (list "" Γ "(" x ")")])]
     ['extend (match-lambda [(list _ _ Γ x t _)   (list "" Γ ", " x ":" t)])]
     ['substitute
-     (match-lambda [(list _ _ e x v _)           (list "" e "[" x ":=" v "]")])]
+             (match-lambda [(list _ _ e x v _)   (list "" e "[" x ":=" v "]")])]
     ['types* (match-lambda [(list _ _ e t _)     (list "" e " : " t)])]
     ['types  (match-lambda [(list _ _ Γ e t _)   (list "" Γ " ⊢ " e " : " t)])])
    (with-atomic-rewriter 't "τ" (begin expr0 expr ...))))
@@ -45,10 +53,10 @@
   (term (types Γ e t)))
 
 (define-syntax-rule (rulename n)
-  (symbol->string 'n))
+  (list "["  (symbol->string 'n) "]"))
 
 (define-syntax-rule (langname n)
-  (symbol->string 'n))
+  (tt (symbol->string 'n)))
 
 (define-syntax-rule (render-reduction-rules rel rule ...)
   (parameterize ([render-reduction-relation-rules '(rule ...)])
@@ -63,3 +71,30 @@
   (parameterize ([render-language-nts '(nt ...)])
     (with-rewriters (centered (render-language lang)))))
 
+(define (theorem-like/thunk kind name body)
+  (if name
+      (list (bold kind) " (" name ")." (body))
+      (list (bold kind ".") (body))))
+
+(begin-for-syntax
+  (define-splicing-syntax-class opt-name
+    [pattern (~seq #:name name:expr)]
+    [pattern (~seq)
+             #:with name #'#false]))
+
+(define-syntax (theorem-like stx)
+  (syntax-parse stx
+    [(_ kind:id :opt-name body:expr ...)
+     #'(theorem-like/thunk 'kind name (λ () "" body ...))]))
+
+(define-syntax (define-theorem-like stx)
+  (syntax-parse stx
+    [(_ fun:id kind:id)
+     #'(define-syntax (fun stx1)
+         (syntax-parse stx1
+           [(_ :opt-name body:expr (... ...))
+            #'(theorem-like kind #:name name body (... ...))]))]))
+
+(define-theorem-like theorem Theorem)
+(define-theorem-like lemma Lemma)
+(define-theorem-like exercise Exercise)
