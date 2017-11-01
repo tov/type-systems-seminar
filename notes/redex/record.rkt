e3ff1c0a6f26e9eed2c84553ca4b52ec349518ed
Jesse A. Tov <jesse.tov@gmail.com>
2017-11-01 10:22:37 -0500

    Records with subtyping

---
+++ notes/redex/record.rkt
@@ -0,0 +1,84 @@
+#lang racket/base
+
+(provide)
+
+(require redex/reduction-semantics
+         (prefix-in stlc: "stlc.rkt")
+         "util.rkt")
+
+(define-extended-language record stlc:stlc
+  (t ::=
+     ....
+     top
+     (Record [f t] ...))
+  (e ::=
+     ....
+     (record [f e] ...)
+     (project e f))
+  (v ::=
+     ....
+     (record [f v] ...))
+  (E ::=
+     ....
+     (record [f v] ... [f E] [f e] ...))
+  (f g ::= variable-not-otherwise-mentioned))
+
+(define ->val
+  (extend-reduction-relation
+   stlc:->val
+   record
+   #:domain e
+   (--> (in-hole E (project (record [f_i v_i] ... [f v] [f_j v_j] ...) f))
+        (in-hole E v)
+        prj)))
+
+(define-metafunction record
+  lookup : Γ x -> t
+  [(lookup (extend Γ x t) x)
+   t]
+  [(lookup (extend Γ y t) x)
+   (lookup Γ x)
+   (side-condition (not (equal? (term x) (term y))))])
+
+(define-extended-judgment-form record stlc:types
+  #:mode (types/alt I I O)
+  #:contract (types/alt Γ e t)
+  
+  [(types/alt Γ v_i t_i)
+   ...
+   ---- record
+   (types/alt Γ (record [f_i v_i] ...) (Record [f_i t_i] ...))]
+
+  [(types/alt Γ e (Record [f_i t_i] ... [f t] [f_j t_j] ...))
+   ---- project
+   (types/alt Γ (project e f) t)])
+
+(define-judgment-form record
+  #:mode (<: I I)
+  #:contract (<: t t)
+
+  [---- nat
+   (<: nat nat)]
+
+  [---- top
+   (<: t top)]
+
+  [(<: t_21 t_11)
+   (<: t_12 t_22)
+   ---- arr
+   (<: (-> t_11 t_12) (-> t_21 t_22))]
+
+  [---- rec-nil
+   (<: (Record [f t] ...) (Record))]
+
+  [(<: t^l t^r)
+   (<: (Record [g_j t_j] ... [g_k t_k] ...) (Record [g_i t_i] ...))
+   ---- rec-cons
+   (<: (Record [g_j t_j] ... [f t^l] [g_k t_k] ...) (Record [f t^r] [g_i t_i] ...))])
+
+(define-judgment-form record
+  #:mode (types I I O)
+  #:contract (types Γ e t)
+
+  [---- var
+   (types Γ x (lookup Γ x))])
