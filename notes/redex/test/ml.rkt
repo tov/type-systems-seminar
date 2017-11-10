15edb50a7719ab4a496097290a8606faec5d75d2
Jesse A. Tov <jesse.tov@gmail.com>
2017-11-09 19:20:04 -0600

    Tests for Algorithm W

--- notes/redex/test/ml.rkt
+++ notes/redex/test/ml.rkt
@@ -6,13 +6,40 @@
          rackunit
          redex/reduction-semantics)
 
+;                                                         
+;                                                         
+;                                                         
+;                                                         
+;                             ;;          ;;;;            
+;                             ;;         ;;;;;            
+;                                        ;;               
+;                                        ;;               
+;   ;;     ;;  ;;  ;;;      ;;;;      ;;;;;;;;  ;;     ;; 
+;   ;;     ;;  ;;;;;;;;       ;;      ;;;;;;;;   ;;   ;;  
+;   ;;     ;;  ;;;   ;;;      ;;         ;;      ;;   ;;  
+;   ;;     ;;  ;;     ;;      ;;         ;;      ;;;  ;;  
+;   ;;     ;;  ;;     ;;      ;;         ;;       ;; ;;   
+;   ;;     ;;  ;;     ;;      ;;         ;;       ;; ;;   
+;   ;;     ;;  ;;     ;;      ;;         ;;        ;;;;   
+;   ;;;   ;;;  ;;     ;;      ;;         ;;        ;;;    
+;    ;;;;;;;;  ;;     ;;      ;;         ;;         ;;    
+;     ;;;  ;;  ;;     ;;   ;;;;;;;;      ;;        ;;;    
+;                                                  ;;     
+;                                                  ;;     
+;                                                ;;;;     
+;                                                ;;;      
+;                                                         
+
+; [List-of A] -> A or #false
+(define (unique-car lst)
+  (cond
+    [(null? lst) #false]
+    [(null? (cdr lst)) (car lst)]
+    [else (fail-check "non-unique result")]))
+
 ; t t -> S or #false
 (define (unify-types t_1 t_2)
-  (define results (judgment-holds (unify ,t_1 ,t_2 S) S))
-  (cond
-    [(null? results) #false]
-    [(null? (cdr results)) (car results)]
-    [(fail-check "Unifies non-uniquely")]))
+  (unique-car (judgment-holds (unify ,t_1 ,t_2 S) S)))
   
 (define-check (check-unifies/fun? t_1 t_2)
   (unless (unify-types t_1 t_2)
@@ -29,6 +56,7 @@
   (check-does-not-unify/fun? (term t_1) (term t_2)))
 
 (define-check (check-unifies-with/fun? t_1 t_2 S)
+  ; S -> [List-of [List a t]]
   (define (subst->list S)
     (match S
       [`•                            `()]
@@ -57,4 +85,78 @@
 (check-unifies-with? a bool ((a bool)))
 (check-unifies-with? bool a ((a bool)))
 (check-unifies-with? a (-> b b) ((a (-> b b))))
-(check-unifies-with? a (-> a b) ())
+(check-unifies-with? (-> a b) (-> (-> b b) (-> c c))
+                     ((a (-> (-> c c) (-> c c)))
+                      (b (-> c c))))
+(check-unifies-with? (-> a (-> c c)) (-> (-> b b) b)
+                     ((a (-> (-> c c) (-> c c)))
+                      (b (-> c c))))
+
+
+;                                                                    
+;                                                                    
+;                                                                    
+;                                                                    
+;               ;;;;                                      ;;       ;;
+;                 ;;                                      ;;       ;;
+;                 ;;                                      ;;       ;;
+;                 ;;                                      ;;;     ;;;
+;     ;;;;;       ;;        ;;;; ;;    ;;;;;               ;; ;;; ;; 
+;    ;;;;;;;;     ;;       ;;;;;;;;   ;;;;;;;              ;; ;;; ;; 
+;    ;     ;;     ;;      ;;;   ;;;  ;;;   ;;;             ;; ; ; ;; 
+;     ;;;;;;;     ;;      ;;     ;;  ;;     ;;             ;; ; ; ;; 
+;    ;;;;;;;;     ;;      ;;     ;;  ;;     ;;             ;; ; ; ;; 
+;   ;;;    ;;     ;;      ;;     ;;  ;;     ;;             ;;;; ;;;; 
+;   ;;     ;;     ;;      ;;     ;;  ;;     ;;             ;;;   ;;; 
+;   ;;    ;;;     ;;      ;;;   ;;;  ;;;   ;;;              ;;   ;;  
+;    ;;;;;;;;      ;;      ;;;;;;;;   ;;;;;;;               ;;   ;;  
+;     ;;;  ;;       ;;;;    ;;;; ;;    ;;;;;                ;;   ;;  
+;                                ;;                                  
+;                          ;    ;;;                                  
+;                          ;;;;;;;                                   
+;                           ;;;;;                                    
+;                                                                    
+
+; e -> σ or #false
+(define (type-check e)
+  (define t (unique-car (judgment-holds (W • ,e S t) t)))
+  (and t (term (gen (ftv ,t) ,t))))
+
+(define-syntax-rule (check-types? e t)
+  (test-equal (type-check (term e)) (term t)))
+
+(define-syntax-rule (check-does-not-type? e)
+  (check-false (type-check (term e))))
+
+(default-language λ-ml)
+
+(check-types? true bool)
+(check-types? (let x true x) bool)
+(check-types? (λ x x) (all a (-> a a)))
+; the order of binders produced by generalization is unpredictable!
+(check-types? (λ x (λ y x)) (all a (all b (-> b (-> a b)))))
+(check-types? (λ f (λ g (λ x (ap f (ap g x)))))
+              (all a (all c (all b (-> (-> b c) (-> (-> a b) (-> a c)))))))
+(check-types? (let f (λ x x) (ap f (λ x (λ y x))))
+              (all a (all b (-> b (-> a b)))))
+(check-types? (let f (λ x x)
+                (if (ap f true)
+                    (ap f true)
+                    true))
+              bool)
+
+(check-types? (λ y
+                (let f (λ x x)
+                  (if y
+                      (ap f true)
+                      (ap (ap f (λ y y)) false))))
+              (-> bool bool))
+              
+(check-does-not-type? (λ x (ap x x)))
+(check-does-not-type? (ap true true))
+
+(check-does-not-type? (λ y
+                        (λ f
+                          (if y
+                              (ap f true)
+                              (ap (ap f (λ y y)) false)))))
