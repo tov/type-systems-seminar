bf117f59201129909b70563f0de7fdfc876fbdfa
Jesse A. Tov <jesse.tov@gmail.com>
2017-12-05 16:30:28 -0600

    Made into Racket package!

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang setup/infotab
+(define name "northwestern type systems seminar")
+(define collection "nu-type-systems")
+(define categories '(educational))
+(define can-be-loaded-with 'all)
+(define required-core-version "6.11")
+(define version "0.0.1")
+(define deps '("turnstile"))
+(define test-omit-paths '("."))
