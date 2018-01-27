0c7dd1a30f6b6def69d92f1ccb1163251ca6e086
Jesse A. Tov <jesse.tov@gmail.com>
2018-01-27 00:06:31 -0600

    More consistent naming.

--- exercises/stlc/eval.ml
+++ exercises/stlc/eval.ml
@@ -18,7 +18,7 @@ let rec string_of_value = function
 
 (* Exception thrown in cases that should not be possible in well-typed
  * programs. *)
-exception Can'tHappen of string
+exception Can't_happen of string
 
 (* Evaluation, the heart of the interpreter: *)
 let rec eval env = function
@@ -33,19 +33,19 @@ let rec eval env = function
   | SubE(e1, e2) ->
       (match eval env e1, eval env e2 with
        | IntV z1, IntV z2 -> IntV (z1 - z2)
-       | _ -> raise (Can'tHappen "ints expected"))
+       | _ -> raise (Can't_happen "ints expected"))
   | If0E(cond, zero, non_zero) ->
       (match eval env cond with
        | IntV 0 -> eval env zero
        | IntV _ -> eval env non_zero
-       | _ -> raise (Can'tHappen "int expected"))
+       | _ -> raise (Can't_happen "int expected"))
   | TupE es ->
       let vs = List.map ~f:(eval env) es in
         TupV vs
   | PrjE(i, e) ->
       (match eval env e with
        | TupV vs -> List.nth_exn vs i
-       | _ -> raise (Can'tHappen "tuple expected"))
+       | _ -> raise (Can't_happen "tuple expected"))
   | LamE(bindings, body) ->
       CloV(env, List.map ~f:(fun (x, _) -> x) bindings, body)
   | AppE(e0, es) ->
@@ -55,11 +55,11 @@ let rec eval env = function
          | CloV(env, xs, body) ->
             let env = Env.extend_lists env xs vs in
               eval env body
-         | _ -> raise (Can'tHappen "closure expected"))
+         | _ -> raise (Can't_happen "closure expected"))
   | FixE(x, (ArrT(ts, _) as t), e) ->
       let ys = Var.fresh_n (List.length ts) (fv e) in
       let v = CloV(env, ys,
                    AppE(FixE(x, t, e), List.map ~f:(fun x -> VarE x) ys)) in
         eval (Env.extend env x v) e
   | FixE(_, _, _) ->
-      raise (Can'tHappen "fix requires 1-arg function type")
+      raise (Can't_happen "fix requires 1-arg function type")
