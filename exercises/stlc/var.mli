fe923c7e7ed62e680a4d8972f3f89b5205bf43e7
Jesse A. Tov <jesse.tov@gmail.com>
2018-01-28 13:12:07 -0600

    Fixed typos.

--- exercises/stlc/var.mli
+++ exercises/stlc/var.mli
@@ -12,9 +12,9 @@ module Set : Core.Set.S with type Elt.t = t
  * generates a fresh name. *)
 val fresh : t -> Set.t -> t
 
-(* Given a list of suggested variable names a set of names to avoid,
+(* Given a list of suggested variable names and a set of names to avoid,
  * generates a list of fresh names of the same length. *)
 val fresh_list : t list -> Set.t -> t list
 
-(* Generates the give number of fresh variable names. *)
+(* Generates the given number of fresh variable names. *)
 val fresh_n : int -> Set.t -> t list
