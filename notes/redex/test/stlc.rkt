406931cb0e07bfd0fce1927a3bf627516564427d
Robby Findler <robby@racket-lang.org>
2025-10-22 08:39:10 -0500

    more tests for sltc

--- notes/redex/test/stlc.rkt
+++ notes/redex/test/stlc.rkt
@@ -70,3 +70,57 @@
 (test-->> ->val/rec
           (term (ap ,fact (s (s (s (s z))))))
           (to-n 24))
+
+(define swap
+  (term (λ n nat (rec n [(s z)] [xpre yrec z]))))
+
+(test-->> ->val/rec
+          (term (ap ,swap z))
+          (term (s z)))
+(test-->> ->val/rec
+          (term (ap ,swap (s z)))
+          (term z))
+
+(define parity
+  (term (λ n nat (rec n [z] [xpre yrec (ap ,swap yrec)]))))
+
+(test-->> ->val/rec
+          (term (ap ,parity z))
+          (term z))
+(test-->> ->val/rec
+          (term (ap ,parity (s z)))
+          (term (s z)))
+(test-->> ->val/rec
+          (term (ap ,parity (s (s z))))
+          (term z))
+(test-->> ->val/rec
+          (term (ap ,parity (s (s (s z)))))
+          (term (s z)))
+
+(define halve
+  (term (λ n nat (rec n [z] [xpre yrec (ap (ap ,add (ap ,parity xpre)) yrec)]))))
+
+(test-->> ->val/rec
+          (term (ap ,halve z))
+          (term z))
+(test-->> ->val/rec
+          (term (ap ,halve (s z)))
+          (term z))
+(test-->> ->val/rec
+          (term (ap ,halve (s (s z))))
+          (term (s z)))
+(test-->> ->val/rec
+          (term (ap ,halve (s (s (s z)))))
+          (term (s z)))
+(test-->> ->val/rec
+          (term (ap ,halve (s (s (s (s z))))))
+          (term (s (s z))))
+(test-->> ->val/rec
+          (term (ap ,halve ,(to-n 9)))
+          (to-n 4))
+(test-->> ->val/rec
+          (term (ap ,halve ,(to-n 10)))
+          (to-n 5))
+(test-->> ->val/rec
+          (term (ap ,halve ,(to-n 11)))
+          (to-n 5))
